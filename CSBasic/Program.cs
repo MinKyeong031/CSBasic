c9e60dcd7790e6762db64070fb2110d508dfccb1
MinKyeong031 <s2019s07@e-mirim.hs.kr>
2021-04-19 15:56:01 +0900

    #8 자료형 검사 GetType()

--- CSBasic/Program.cs
+++ CSBasic/Program.cs
@@ -145,8 +145,21 @@ namespace CSBasic
             Console.WriteLine(number);
             Console.WriteLine(number);
 
+            int _int = 273;
+            long _long = 624235252342;
+            float _float = 52.273F;
+            double _double = 52.273;
+            char _char = '글';
+            string _string = "문자열";
 
-
+            Console.WriteLine((273).GetType());
+            Console.WriteLine(_int.GetType());
+            Console.WriteLine(_long.GetType());
+            Console.WriteLine(_float.GetType());
+            Console.WriteLine(_double.GetType());
+            Console.WriteLine(_char.GetType());
+            Console.WriteLine(_string.GetType());
+            Console.WriteLine((2211111111L).GetType());
         }
     }
 }
